4cdcd4584af8f893708d51e5a24836a7634d6f0b
Jose Tellez <tellezjose@hotmail.com>
2023-09-20 07:46:47 -0700

    write function for simple console app

--- KubernetesExamples/SimpleConsoleApp/Program.cs
+++ KubernetesExamples/SimpleConsoleApp/Program.cs
@@ -4,7 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string? name = Environment.GetEnvironmentVariable("POD_NAME");
+
+        Write("data", "file.txt", name);
+    }
+
+    private static void Write(string folderName, string fileName, string? consoleAppName)
+    {
+        if (string.IsNullOrEmpty(consoleAppName))
+            consoleAppName = "Default";
+
+        string folderPath = $"{Environment.CurrentDirectory}/{folderName}";
+
+        string filePath = Path.Combine(folderPath, fileName);
+
+        try
+        {
+            Directory.CreateDirectory(folderPath);
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{consoleAppName} wrote on {DateTime.Now}");
+            }
+
+            Console.WriteLine($"{consoleAppName} wrote to {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{consoleAppName} has error: {ex.Message}");
+        }
     }
 }
 
